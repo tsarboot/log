e451fef5a71e8e07f699c48720289067bd618f54
lol <you@lol.com>
2020-09-29 05:40:10 -0700

    changed

---
+++ models/users.js
@@ -0,0 +1,16 @@
+const Mongoose = require("mongoose");
+const userSchema = new Mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = Mongoose.model("users", userSchema);
